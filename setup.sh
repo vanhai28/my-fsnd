0b349a757ce761c337ef4bfbb83c04380442ae6c
nguyễn văn hải <nvhaik18@gmail.com>
2022-08-22 22:54:18 +0700

    fix build

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
+export DATABASE_URL="postgres://tzvhprqssljaak:34017426811808f9de691180a916ab2fcde38f4cdc533029aba6bc95260e1c6c@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d6kuteh2815ltv"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
